dd1b48c9a38e1fff7169986378c84edcebed6248
Tithi Paul <tithi.p207@gmail.com>
2026-06-28 15:31:24 -0400

    Clock

--- dashboard/dashboard.js
+++ dashboard/dashboard.js
@@ -1,1 +1,30 @@
-console.log("Dashboard script loaded");
+// top bar
+
+//clock
+
+function updateClock() {
+  const now = new Date();
+  let hour = now.getHours();
+  let minutes = now.getMinutes();
+
+  if (hour < 10) {
+    hour = "0" + hour;
+  }
+
+  if (minutes < 10) {
+    minutes = "0" + minutes;
+  }
+
+  // removing military time
+  if (hour > 12) {
+    hour -= 12;
+  }
+
+  const timeString = `${hour} : ${minutes}`;
+
+  document.getElementById("clock").textContent = timeString;
+
+}
+
+updateClock();
+setInterval(updateClock, 6000);
